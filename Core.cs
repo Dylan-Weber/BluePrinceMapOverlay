f9d5e780cae5a282984fbaec005959044cc490ec
Dylan Weber <34782089+Dylan-Weber@users.noreply.github.com>
2025-07-03 21:34:14 -0400

    Initial commit.

---
+++ Core.cs
@@ -0,0 +1,15 @@
+﻿using MelonLoader;
+
+[assembly: MelonInfo(typeof(BluePrinceMapOverlay.Core), "BluePrinceMapOverlay", "1.0.0", "dylan", null)]
+[assembly: MelonGame("Dogubomb", "BLUE PRINCE")]
+
+namespace BluePrinceMapOverlay
+{
+    public class Core : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Initialized.");
+        }
+    }
+}
